14d8b2c2f993a282c2325c6375dcb968d040dec7
BB pezsgő <bazsi5fel2@gmail.com>
2024-01-25 16:12:05 +0100

    what?

--- build.ps1
+++ build.ps1
@@ -5,4 +5,4 @@ if (Test-Path -Path ./Published)
 
 (Get-Content "./YeahGame/YeahGame.csproj") -replace '<PublishAot>true</PublishAot>', '<PublishAot>false</PublishAot>' | Set-Content "./YeahGame/YeahGame.csproj"
 
-dotnet publish ./YeahGameWeb/YeahGameWeb.csproj --configuration Release --runtime browser-wasm --self-contained true -p:PublishTrimmed=true --output ./Published
+dotnet publish ./YeahGameWeb/YeahGameWeb.csproj --configuration Release --runtime browser-wasm --self-contained true -p:PublishTrimmed=true -p:PublishAot=false --output ./Published
