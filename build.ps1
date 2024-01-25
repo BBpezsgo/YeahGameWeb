ed10087bf29c017e248d57c3db439ff1f120fd1d
BB pezsgő <bazsi5fel2@gmail.com>
2024-01-25 09:06:11 +0100

    i think its working

--- build.ps1
+++ build.ps1
@@ -3,4 +3,6 @@ if (Test-Path -Path ./Published)
     Remove-Item -LiteralPath ./Published -Force -Recurse
 }
 
+(Get-Content "./YeahGame/YeahGame.csproj") -replace '<PublishAot>true</PublishAot>', '<PublishAot>false</PublishAot>' | Set-Content "./YeahGame/YeahGame.csproj"
+
 dotnet publish ./YeahGameWeb/YeahGameWeb.csproj --configuration Release --runtime browser-wasm --self-contained true -p:PublishTrimmed=true --output ./Published
